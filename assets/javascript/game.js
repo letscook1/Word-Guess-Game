14af3d176584e2a68fafad0e774ca5ea919bc97e
letscook1 <letscook1@hotmail.com>
2019-02-08 17:07:23 -0500

    adding more stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+//grab reference to my dom elements
+var domStartButton = document.getElementsByClassName(start - button);
+var domLetterSpots = docucument.getElementsByClassName(letter - Spots);
+var domGuesses = document.getElementById(guesses);
+var domGuessesLeft = document.getElementById(gusses - left);
+var domWins = document.getElementById(wins);
+var domLosses = document.getElementById(losses);
+//create variables for game (wordBank, wins, losses, picked word, guesses left, game running, picked word placeholder, guessed letter bank, incorrect letter bank)
+var wordBank = [
+  "Octopus",
+  "Coral Reef",
+  "Manatee",
+  "Starfish",
+  "Killer Whale",
+  "Beluga Whale",
+  "Humpback Whale",
+  "Galapagos Tortoise",
+  "Seahorse",
+  "Mermaid",
+  "California Seal",
+  "Clown Fish",
+  "JellyFish",
+  "Stingray",
+  "Dolphin",
+  "Sea Otter",
+  "Sea Lion",
+  "Walrus",
+  "Penguin",
+  "Axolotl",
+  "Lobster",
+  "Electric Eel",
+  "Aqua Man"
+];
+
+//newGame function to reset all stats, pick new word and create placeholders
+
+// letter guess function, takes in the letter you pressed and sees if it's in the selected word
+
+// checkIncorrect(letter)
+
+//checkLose
+
+//checkWin
+
+//add event listener for new game button
+
+//add onkeyupevent to trigger letterGuess
