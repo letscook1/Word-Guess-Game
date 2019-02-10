22007b5cc6aa6e7a78480a6fb4839ada70f3c622
letscook1 <letscook1@hotmail.com>
2019-02-10 14:08:37 -0500

    added slideshow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,36 @@
-//DOM element variables
+//Beginning Slide Show
 
-var startButtonDOM = document.getElementById(start_button);
-var blankLettersDOM = document.getElementById(letter_sports);
-var wrongLettersArrayDOM = document.getElementById(wrong_letter_bank);
-var remainingGuessesDOM = document.getElementById(remaining_guess_bank);
-var winsDOM = document.getElementById(win_bank);
-var lossesDOM = document.getElementById(loss_bank);
+var i = 0; //start point
+var images = []; //images array
+var time = 3000; //time between switch
 
-//create an array of words
-var seaLifeWords = [
-  "Octopus",
-  "Coral Reef",
-  "Manatee",
-  "Starfish",
-  "Killer Whale",
-  "Beluga Whale",
-  "Humpback Whale",
-  "Galapagos Tortoise",
-  "Seahorse",
-  "Mermaid",
-  "California Seal",
-  "Clown Fish",
-  "JellyFish",
-  "Stingray",
-  "Dolphin",
-  "Sea Otter",
-  "Sea Lion",
-  "Walrus",
-  "Penguin",
-  "Axolotl",
-  "Lobster",
-  "Electric Eel",
-  "Aqua Man"
-];
+//image list
+images[0] = "assets/images/slide1.jpg";
+images[1] = "assets/images/slide2.jpg";
+images[2] = "assets/images/slide3.jpg";
+images[3] = "assets/images/slide4.jpg";
+images[4] = "assets/images/slide5.jpg";
+images[5] = "assets/images/slide6.jpg";
+images[6] = "assets/images/slide7.jpg";
+images[7] = "assets/images/slide8.jpg";
+images[8] = "assets/images/slide9.jpg";
 
-//pick a random word from the words array
-var word = seaLifeWords[Math.floor(Math.random() * seaLifeWords.length)];
+//change image
+function changeImg() {
+  document.slide.src = images[i];
 
-//sets up the answerArray to show how many letters there are using _'s
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-}
+  //check if index is under max
 
-//do...
-//some way to push that to the dom element.
-
-//create a variable to hold the number of remainingLetters to be guessed
-var blankLetters = word.length;
-
-//do..
-//not really sure where the heck this would be lol.
-
-//  ****************** THE MAIN GAME LOOP +++++++++++++++
-
-//while there are letters left to be guessed
-while (blankLetters > 0) {
-  //show the player their progress
-  alert(answerArray.join(" "));
-
-  //do..
-  //domElement.textContent = answerArray.join(" ");
-  //or push????
-
-  //get a guess from the player
-  var guess = prompt(" Guess a letter, or click Cancle to stop playing.");
-
-  //do...
-  //just put that in the HTML/CSS place
-  //then domElement.textConent = guess.join(" ");
-
-  //if the guess is blank
-  if (guess == null) {
-    //exit the game loop
-    break;
-    //if the guess is more than one letter or no letters
-  } else if (guess.length !== 1) {
-    //alert them to guess a single letter only;
-    alert("Please enter a single letter.");
-    //valid guess
+  if (i < images.length - 1) {
+    //ad 1 to index
+    i++;
   } else {
-    //update the game state with the guess
-    for (var j = 0; j < word.length; j++) {
-      //if the letter they guessed is in the word at that point or index
-      if (word[j] === guess) {
-        //update the answer array with the letter they guessed at that point or index
-        answerArray[j] = guess;
-        //substract one from remaining letters
-        remainingLetters--;
-      }
-    }
+    //reset back to 0
+    i = 0;
   }
-  // ************ END OF THE GAME LOOP ***********
+  //run function every x seconds
+  setTimeout("changeImg()", time);
 }
-//let player know the word
-alert(answerArray.join(" "));
 
-//do..
-//domElement.textContent.
-
-//do...
-//start with only 8 possible guesses for player
-//switch picture for each guess loss
-
-//contrate the player
-//only do it if remaining guesses is >0
-//probably have if remaining guesses = some number then have a certain picture play
-alert("Good Job! Good Effort! the answer was " + word);
-//do...
-//switching screen to whole scuba diver and then start game again with shark
-//have a clapping sound
-//if...
-//remaining guesses = 0
-//go to screen with only shark and "you loose"
-//have some sort of "awwww" sound lol
-
-//do...
-//have a function to add to wins
-
-//do...
-//have a function to add to losses
+//run function when page loads
+window.onload = changeImg;
